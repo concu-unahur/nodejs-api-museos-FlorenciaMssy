fccf1d6cd401c8c17f2ffd4fc6a1b89ad9f1d7a1
FlorenciaMssy <florenciamssy@gmail.com>
2020-03-10 20:18:26 -0300

    falta

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const superagent = require('superagent');
+const fs = require('fs')
 
 function imprimirMuseos(error, respuesta) {
   if (error) {
@@ -12,11 +13,30 @@ function imprimirMuseos(error, respuesta) {
   console.log(`El primer museo se llama ${museos[0].nombre}.`)
 }
 
+function escribirArchivo(error, respuesta){
+
+  fs.writeFile('./museos.txt, El nombre del primer museo es: ', imprimirMuseos)
+}
+
+function terminar(error){
+
+  if(error){
+    throw new Error ('Hubo un problema');
+
+  }
+  else {
+    console.log('Termine')
+
+  }
+  
+  }
+
+  console.log('Termine')
 console.log('Antes de llamar a superagent')
 
 superagent
   .get('https://www.cultura.gob.ar/api/v2.0/museos')
   .query({ format: 'json' })
-  .end(imprimirMuseos)
+  .end(escribirArchivo)
 
 console.log('Después de llamar a superagent')
