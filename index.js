522ed57ed9e31db4b7d612e77dc58c55f0afa481
FlorenciaMssy <florenciamssy@gmail.com>
2020-03-12 20:58:40 -0300

    falta

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
 const superagent = require('superagent');
-const fs = require('fs')
+const fs = require('fs');
 
-function imprimirMuseos(error, respuesta) {
+function escribirArchivoMuseos(error, respuesta) {
+    if (error) {
+    throw new Error('algo se rompió', error);
+  }
+
+  const museos = respuesta.body.results;
+
+  museos.forEach( museo =>{ actualMuseos = `El museo es: ${museo.nombre}. Direccion: ${museo.direccion}. Por cualquier consulta comunicarse al: ${museo.telefono}\n`;
+  fs.appendFile('./museos.txt',actualMuseos,end);})
+}
+
+function escribirArchivoOrganismos(error,respuesta){
+
   if (error) {
     throw new Error('algo se rompió', error);
   }
 
-  const cantidad = respuesta.body.count;
-  const museos = respuesta.body.results;
+  const organismos = respuesta.body.results;
 
-  console.log(`Se encontraron ${cantidad} museos.`);
-  console.log(`El primer museo se llama ${museos[0].nombre}.`)
-}
+  organismos.forEach( organismo =>{ actualOrganismos = `El organismo es: ${organismo.nombre}. Direccion: ${organismo.direccion}. Por cualquier consulta comunicarse al: ${organismo.telefono}\n`;
+  fs.appendFile('./organismos.txt',actualOrganismos,end);})
 
-function escribirArchivo(error, respuesta){
-
-  fs.writeFile('./museos.txt, El nombre del primer museo es: ', imprimirMuseos)
-}
-
-function terminar(error){
-
-  if(error){
-    throw new Error ('Hubo un problema');
-
-  }
-  else {
-    console.log('Termine')
-
-  }
   
-  }
+}
 
-  console.log('Termine')
-console.log('Antes de llamar a superagent')
+function end(error){
+  if (error) {
+    throw new Error('algo se rompió', error);
+  }
+  else{
+    console.log('Terminado.');
+  }
+}
+
+//console.log('Antes de llamar a superagent')
+
+
 
 superagent
   .get('https://www.cultura.gob.ar/api/v2.0/museos')
   .query({ format: 'json' })
-  .end(escribirArchivo)
+  .end(escribirArchivoMuseos) //el query le pasa la data a esta funcion
 
-console.log('Después de llamar a superagent')
+
+  superagent
+  .get('https://www.cultura.gob.ar/api/v2.0/organismos')
+  .query({ format: 'json' })
+  .end(escribirArchivoOrganismos) //el query le pasa la data a esta funcion
